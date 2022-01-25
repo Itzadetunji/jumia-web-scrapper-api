aaf62b556bb6720b33e07565ef937fc03cc5c7ce
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-01-25 18:17:18 +0100

    Updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ axios(url)
   .then(response => {
     const html = response.data
     const $ = cheerio.load(html)
-    ${'.'}
+    $('.fc-item__title', html).each(function)
   })
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
