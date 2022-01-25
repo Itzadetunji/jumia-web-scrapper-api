610a59d391ed4c64dbeb6b735e06ba18f0375e80
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-01-25 18:10:53 +0100

    Updated

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ const express = require('express');
 
 const app = express()
 
+const url = 'https://www.theguardian.com/uk'
+
+axios(url);
+
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
